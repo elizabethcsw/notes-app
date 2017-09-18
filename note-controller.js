0a1451c4a439a887a5a93699ba94bae49655396a
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-09-18 01:35:26 +0100

    step 15 prep

--- note-controller.js
+++ note-controller.js
@@ -15,32 +15,7 @@ function howdy(){
 //   var element = document.getElementById('app');
 //   element.innerHTML = notelistview.view()
 // };
-(function(exports) {
-  function Listen(evt) {
-    evt.preventDefault();
-    console.log("first");
-    var newnote = document.getElementById('new').value;
-    notecontroller.notelistview.noteList.add(new Note(newnote));
-    // notecontroller.insertHTML(this)
-    document.getElementById('listnotes').value = 'Back';
-    document.getElementById('app').innerHTML = notecontroller.notelistview.view();
 
-
-    document.getElementById('new').value = ""
-    // document.getElementById('new').addEventListener('submit', function(evt){
-    // evt.preventDefault();
-    // console.log("inside")
-    // var newnote = document.getElementById('new').value
-    // notecontroller.notelistview.noteList.add(new Note(newnote))
-    // });
-    // var newnote = document.forms["myForm"]["newnote"].value
-
-    console.log("outside")
-  };
-
-
-  exports.Listen = Listen;
-})(this);
 
 (function(exports) {
   function NoteController(notelist) {
@@ -71,23 +46,25 @@ function howdy(){
         console.log("Here2:" + location.pathname.split("notes-app/")[1])
         console.log("Here3:" + location.hash)
         element.innerHTML = notelistview.view();
-      }
+      };
 
 
       // element.innerHTML = notelistview.view();
+      this.checkhash = function(){
+        window.onhashchange = function(){
+          if(location.hash!== ""){
+            var id = window.location.hash.split("#")[1].split("/")[1];
+            var singlenote = new SingleNoteView(notelistview.noteList.list[id])
+            document.getElementById("app").innerHTML = singlenote.view();
+            anything.value = 'List Notes';
+          }
+            // .innerHTML = notelistview.noteList.list[id].readText();
+        };
 
-      window.onhashchange = function(){
-        if(location.hash!== ""){
-          var id = window.location.hash.split("#")[1].split("/")[1];
-          var singlenote = new SingleNoteView(notelistview.noteList.list[id])
-          document.getElementById("app").innerHTML = singlenote.view();
-          anything.value = 'List Notes';
-        }
-          // .innerHTML = notelistview.noteList.list[id].readText();
-
+        return element.innerHTML
       };
 
-      return element.innerHTML
+      this.checkhash();
     };
 
 
@@ -96,7 +73,31 @@ function howdy(){
 
 
 
+(function(exports) {
+  function Listen(evt) {
+    evt.preventDefault();
+    console.log("first");
+    var newnote = document.getElementById('new').value;
+    notecontroller.notelistview.noteList.add(new Note(newnote));
+    // notecontroller.insertHTML(this)
+    document.getElementById('listnotes').value = 'Back';
+    document.getElementById('app').innerHTML = notecontroller.notelistview.view();
+    document.getElementById('new').value = "";
+    notecontroller.checkhash();
+    // document.getElementById('new').addEventListener('submit', function(evt){
+    // evt.preventDefault();
+    // console.log("inside")
+    // var newnote = document.getElementById('new').value
+    // notecontroller.notelistview.noteList.add(new Note(newnote))
+    // });
+    // var newnote = document.forms["myForm"]["newnote"].value
 
+    console.log("outside")
+  };
+
+
+  exports.Listen = Listen;
+})(this);
 
 
 
